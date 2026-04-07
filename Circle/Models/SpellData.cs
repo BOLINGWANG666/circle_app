8321449742d433960386c8d9dc3f6bdb8e012093
WANG <2482635815@qq.com>
2026-04-07 13:43:35 +0800

    feat:add a shaking function when you are hit,and change the data structure by using spell and json

---
+++ Circle/Models/SpellData.cs
@@ -0,0 +1,11 @@
+﻿namespace Circle.Models;
+
+public class SpellData
+{
+    public required string Id { get; set; }
+    public required string Icon { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string EffectType { get; set; }
+    public double Value { get; set; }
+}
