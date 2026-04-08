5975f4e8a533c95b333765ab7d3a36f079f5a107
WANG <2482635815@qq.com>
2026-04-08 16:47:36 +0800

    feat:add a dodge function

--- Circle/Models/PlayerSaveData.cs
+++ Circle/Models/PlayerSaveData.cs
@@ -13,6 +13,8 @@ public class PlayerSaveData
     public int MaxHp { get; set; }
     public int Atk { get; set; }
     public double Cd { get; set; }
+
+    public double DodgeChance { get; set; } = 0.05;
     public int Level { get; set; }
     public int Exp { get; set; }
     public double TimeRemaining { get; set; } = 60.0;
